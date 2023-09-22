8e7fa4b8f7fa1e0a729d29905bc18d1ff17f847d
기우현 <apple09072@vaiv.kr>
2023-09-22 15:08:36 +0900

    최적화1 - useMemo

--- src/App.js
+++ src/App.js
@@ -106,9 +106,9 @@ function App() {
     // }
 
     const getDiaryAnalysis = useMemo(() => {
-        // if (data.length === 0) {
-        //     return { goodcount: 0, badCount: 0, goodRatio: 0 };
-        // }
+        if (data.length === 0) {
+            return { goodCount: 0, badCount: 0, goodRatio: 0 };
+        }
         console.log("일기 분석 시작");
 
         const goodCount = data.filter((it) => it.emotion >= 3).length;
