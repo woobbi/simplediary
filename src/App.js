f41a25a7a96f1cb005f32670440b93a908daae0a
기우현 <apple09072@vaiv.kr>
2023-09-22 16:22:57 +0900

    최적화2 - React.memo(컴포넌트 재사용)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import {useEffect, useMemo, useRef, useState} from "react";
-import LifeCycle from "./LifeCycle";
-import OptimizeTest from "./OptimizeTest";
+// import LifeCycle from "./LifeCycle";
+// import OptimizeTest from "./OptimizeTest";
 
 // https://jsonplaceholder.typicode.com/comments
 
@@ -118,7 +118,7 @@ function App() {
 
     return (
         <div className="App">
-            <OptimizeTest></OptimizeTest>
+            {/*<OptimizeTest></OptimizeTest>*/}
             {/*<LifeCycle></LifeCycle>*/}
             <DiaryEditor onCreate={onCreate}></DiaryEditor>
             <div>전체 일기 : {data.length}</div>
